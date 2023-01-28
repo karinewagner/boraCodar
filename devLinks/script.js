ab2375bc5ebc939f957ab227af767f8b347f5e10
KarineWagner <karinedwagner@gmail.com>
2023-01-28 12:35:19 -0400

    atualização do projeto dev-links

--- devLinks/script.js
+++ devLinks/script.js
@@ -6,8 +6,8 @@ function toggleMode() {
   const img = document.querySelector('.profile img')
 
   if(html.classList.contains('light')) {
-    /* setAttribute serve para adicionar, ajustar, modificar um atributo*/
     img.setAttribute('src', 'https://github.com/karinewagner.png')
+    
   } else {
     img.setAttribute('src', 'https://github.com/karinewagner.png')
   }
